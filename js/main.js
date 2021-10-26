83bfa7f85db65ff20b05f6aa656b83a72a6d3221
elepot87 <78643839+elepot87@users.noreply.github.com>
2021-10-26 16:57:59 +0200

    add js but it is not working

---
+++ js/main.js
@@ -0,0 +1,42 @@
+// Mail
+// - crea una lista contenente almeno 3 indirizzi email
+// - chiedi all’utente la sua email,
+// - controlla che sia nella lista di chi può accedere (lista creata sopra al punto 1),
+// - stampa un messaggio appropriato sull’esito del controllo.
+
+const userEmail = [
+  "eleonorapotente@gmail.com",
+  "info@verdepastello.com",
+  "alice@verdepastello.com",
+  "falace@centroleonardo.net",
+];
+
+const inputEmail = document.getElementById("mail");
+const checkEmail = document.querySelector(".btn");
+
+// Controllo l'indirizzo dell'utente al click del bottone
+
+checkEmail.addEventListener("click", function () {
+  // Ottenere il valore dell'input inserito da utente
+  const user = inputEmail.value.trim().toLowerCase();
+  //   console.log(user);
+
+  //   Controllare che non sia stringa vuota
+  if (user === "") {
+    alert("Inserire una mail per favore!");
+  } else {
+    //   Controllare se la mail inserita è nella lista(Array) userEmail
+    let userFound = false;
+    for (let i = 0; i < userEmail.lenght; i++) {
+      if (userEmail[i] === user) {
+        userFound = true;
+        break;
+      }
+    }
+    if (userFound === true) {
+      alert("Sei nel database");
+    } else {
+      alert("Devi prima registrarti al sito!");
+    }
+  }
+});
